dc07cc0ca814c0d76c9103e02a684523af783014
yariklaz <lazyrkoy@gmail.com>
2026-03-10 08:41:34 +0200

    Add files via upload

---
+++ QuestBooking.Domain/Model/Questroom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuestBooking.Domain.Model
+{
+    public partial class Questroom : Entity
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int MaxPlayers { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public int? DurationMinutes { get; set; }
+
+        public virtual ICollection<Timeslot> Timeslots { get; set; } = new List<Timeslot>();
+    }
+}
