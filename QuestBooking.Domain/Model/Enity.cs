b969a136ed8d4ce07c797c4b4ec7371bc581a9dc
yariklaz <lazyrkoy@gmail.com>
2026-03-17 08:32:38 +0200

    Delete QuestBooking.Domain directory

--- QuestBooking.Domain/Model/Enity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace QuestBooking.Domain.Model
-{
-    public abstract class Entity // Абстрактний клас [cite: 1400]
-    {
-        public int Id { get; set; } // Спільний ідентифікатор [cite: 1410]
-    }
-}
